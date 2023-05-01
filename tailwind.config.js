33ef0bf0bf08f941dda4f8c254441c8e22e175b7
tanvir-hossen49 <thossen49@gmail.com>
2023-05-01 23:44:11 +0600

    initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin")],
+};
