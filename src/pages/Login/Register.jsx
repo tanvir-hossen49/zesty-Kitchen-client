5efd7fb6f7c81f1a3ea01586eb1742a5f1b41240
tanvir-hossen49 <thossen49@gmail.com>
2023-05-02 20:12:58 +0600

    added register page

--- src/pages/Login/Register.jsx
+++ src/pages/Login/Register.jsx
@@ -6,15 +6,25 @@ import { Link } from "react-router-dom";
 import GoogleButton from "react-google-button";
 import GithubButton from "react-github-login-button";
 
-const Login = () => {
+const Register = () => {
   return (
     <Form className="container mt-4 login-form">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3">
+        <Form.Label>Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter Name" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Photo URL</Form.Label>
+        <Form.Control type="text" placeholder="Enter Photo URL" />
+      </Form.Group>
+
+      <Form.Group className="mb-3">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
@@ -22,7 +32,7 @@ const Login = () => {
       <Button className="button w-100">Submit</Button>
 
       <p className="text-center mt-3">
-        New to ZestyKitchen ? <Link to="/register">Create an account</Link>
+        Already have an account? <Link to="/login">Login</Link>
       </p>
 
       <div className="divider d-flex align-items-center justify-content-center mt-4">
@@ -37,4 +47,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Register;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginLayout from "../layouts/LoginLayout";
 import Login from "../pages/Login/Login";
+import Register from "../pages/Login/Register";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "login",
         element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
       },
     ],
   },
